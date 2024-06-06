1ebd1b3b9e0623d8730f80f1b2fe8bf5d9625dd6
Crazy Urus <crazyurus@vip.qq.com>
2024-06-06 10:46:57 +0800

    refactor: moonshot 使用专有 model

--- src/platform/moonshot.ts
+++ src/platform/moonshot.ts
@@ -1,3 +1,7 @@
+import type { BaseLanguageModel } from '@langchain/core/language_models/base';
+
+import { ChatMoonshot } from '@langchain/community/chat_models/moonshot';
+
 import Platform from './base';
 
 class Moonshot extends Platform {
@@ -6,6 +10,15 @@ class Moonshot extends Platform {
   public name = 'Moonshot';
 
   protected server = 'api.moonshot.cn';
+
+  protected getChatModel(apiKey?: string, streaming?: boolean): BaseLanguageModel {
+    return new ChatMoonshot({
+      apiKey,
+      model: this.model,
+      streaming,
+      temperature: this.temperature
+    });
+  }
 }
 
 export default Moonshot;
